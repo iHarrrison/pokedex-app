a86991d048d4b62ad89fa3a71830e555194d30fc
harrisondickens <harrison.dickens0122@gmail.com>
2023-06-09 14:12:53 +0100

    does some quick and dirty CSS clean up

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { TextField } from '@mui/material';
 
-
 const SearchBar = ({ value, onChange }) => {
     return (
-        <TextField label="Search Pokemon" value={value} onChange={onChange} />
+        <TextField
+            label="Search Pokemon"
+            value={value}
+            onChange={onChange}
+            sx={{ width: '30%' }}
+        />
     );
 };
 
